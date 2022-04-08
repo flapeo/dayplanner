d2b19d52e2d8588d8c804277785f614958fd74ff
Brayden Bjorndahl <spankyray23@gmail.com>
2022-04-07 23:50:02 -0700

    idk

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ var m = moment();
 $("#currentDay").html("<h2>" + m.format("dddd ll") + "</h2>")
 function info () {
     var userdata = JSON.parse(localStorage.getItem('userdata'))
+  
     console.log(userdata)
+    
     
 }
 saveBtn.on("click",function () {    
-  var hour = $(time).parent().attr("id");
-  
+  var value = $(this).parent().attr("id");
+  var time = $(this).siblings("ran").val();
   console.log(time)
+  console.log(value)
+
+
+  
+  localStorage.setitem(time, value);
   
 
 
